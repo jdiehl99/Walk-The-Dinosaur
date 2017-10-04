c80953b08ec8d791a7929cf535a8f787061c8bd1
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-04 19:28:20 -0400

    have add dino function working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,3 +1,11 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+var dinoLeft = [];
+var dinoRight = [];
+
 function apiStuff(app, __dirname) {
     const dino1 = require("./../data/newDino.js");
     const dino2 = require("./../data/walkedDino.js");
@@ -5,28 +13,22 @@ function apiStuff(app, __dirname) {
     //  add new dino to left array
     app.post("/dino/new", function (req, res) {
 
-        // take user inputs and shove them into an array
+        // take user dinosaur and puts it into left array
         var formData = req.body;
-        console.log("form data", formData);
-        dino1.push(formData);
-
-        $("#dinolist").html('<h2>Dinos to Walk</h2>');
-        for (i=0;i<dino1.length;i++) {
-            $("#dinolist").append([i] + dino[i][1] + '<button id="donetb" dinodid='+[i]+'">Walk the dinosaur!</button>"');
-        }
-        
+        dinoLeft.push(formData);
+        res.json(dinoLeft);
 
     });
 
     //  move dino to right array
-    app.post("/dino/done", function (req, res) {
+    // app.post("/dino/done", function (req, res) {
 
-        // take user inputs and shove them into an array
-        var formData = req.body;
-        console.log("form data", formData);
-        dino2.push(formData);
+    //     // take user inputs and shove them into an array
+    //     var formData = req.body;
+    //     console.log("form data", formData);
+    //     dino2.push(formData);
 
-    });
+    // });
 }
 
 module.exports = apiStuff;

e66851a33ffcb32fb6c1a8f7a08cc7f17aba872b
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-10-03 21:21:30 -0400

    setup routes to /dino/new and dino/done

---
+++ dinosaur.js
@@ -0,0 +1,26 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+
+var dinoToWalk = [];
+var dinoWalked = [];
+
+function showPage(app, __dirname) {
+
+
+    // show main page
+    app.get("/", function (req, res) {
+        res.render("index", lunches[0]);
+    });
+    // add new dino to list, left side, dinoToWalk array
+    app.get("/dinonew", function (req, res) {
+        res.render("index", lunches[0]);
+    })
+    // move dino to right side, dinoWalked array
+    app.get("/dinodone", function (req, res) {
+        res.render("index", lunches[0]);
+    })
+}
+
+module.exports = showPage;
--- server.js
+++ server.js
@@ -6,11 +6,15 @@ const exphbs = require("express-handlebars");
 const app = express();
 const PORT = process.env.PORT || 4100;
 
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use(bodyParser.urlencoded({extended: true }));
 app.use(bodyParser.json());
 app.use(express.static(__dirname, + '/public'));
 
 var dinos = require("./dinosaur.js");
+dinoDo(app, __dirname);
 
 app.listen(PORT, function() {
     console.log("app listining on PORT " + PORT);
